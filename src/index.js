8dc9db19d7872ba37bb0de702ee8d071beca53ad
Jaime Molina <js.molina13@uniandes.edu.co>
2018-09-03 12:30:03 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App/index.js";
+
+const ROOT = document.querySelector("#accomodationApp");
+
+ReactDOM.render(<App />, ROOT);
