fa7932d980ab73518f33aa938c8f7a5997afd06e
Jaime Molina <js.molina13@uniandes.edu.co>
2018-09-04 18:31:10 -0500

    Liked heart

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,9 +2,42 @@ import React, { Component } from "react";
 import injectSheet from "react-jss"
 import styles from "./styles";
 
+import heartBlank from "../../icons/heartBlank.svg";
+import heartFilled from "../../icons/heartFilled.svg";
+import placeholder from "../../icons/placeholder.svg";
+
+function heartType(liked, classes, _onTouch) {
+    let classesStr = "";
+    let icon = null;
+    if (liked) {
+        classesStr = `${classes["heart--liked"]} ${classes.logo} ${classes.topItems}`;
+        icon = heartFilled;
+    } else {
+        classesStr = `${classes["heart--unliked"]} ${classes.logo} ${classes.topItems}`;
+        icon = heartBlank;
+    }
+
+    return (
+        <div
+            className={classesStr}
+            dangerouslySetInnerHTML={{ __html: icon }}
+            onTouchEnd={_onTouch}
+        />
+    );
+}
+
 class Card extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            liked: false
+        }
+        this._onTouch = this._onTouch.bind(this);
+    }
+
+    _onTouch() {
+        const liked = !this.state.liked;
+        this.setState({ liked });
     }
 
     render() {
@@ -12,7 +45,12 @@ class Card extends Component {
         return (
             <li className={classes.card}>
                 <label className={`${classes.city} ${classes.topItems}`}>{accomodation.name}</label>
-                <label className={`${classes.country} ${classes.topItems}`}>{accomodation.country}</label>
+                <div className={`${classes.country} ${classes.topItems}`}>
+                    <div className={classes.pointer} dangerouslySetInnerHTML={{ __html: placeholder }} />
+                    <label>{accomodation.country}</label>
+                </div>
+
+                {heartType(this.state.liked, classes, this._onTouch)}
                 <img
                     className={classes.imageBottom}
                     src={accomodation.image_urls[0]} alt={accomodation.name}
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,9 +1,48 @@
+const iconSize = "18px";
 const styles = {
     card: {
         listStyleType: "none",
         margin: "16px 0",
         position: "relative",
     },
+    city: {
+        fontSize: "48px",
+        transform: "translate(36px, 96px)",
+    },
+    country: {
+        display: "flex",
+        fontSize: "16px",
+        transform: "translate(36px, 146px)",
+
+        "& svg": {
+            fill: "currentColor"
+        }
+    },
+
+    pointer: {
+        height: iconSize,
+        width: iconSize,
+        marginRight: iconSize,
+    },
+
+    logo: {
+        height: iconSize,
+        width: iconSize,
+        transform: "translateY(12px)",
+        right: "12px",
+
+        "& svg": {
+            fill: "currentColor"
+        }
+    },
+    "heart--liked": {
+        color: "pink !important",
+    },
+
+    "heart--unliked": {
+        color: "white",
+    },
+
     imageBottom: {
         height: "180px",
         width: "100%",
@@ -11,19 +50,13 @@ const styles = {
         zIndex: 0,
 
     },
+
     topItems: {
         position: "absolute",
         zIndex: 1,
         color: "white"
     },
-    city: {
-        fontSize: "48px",
-        transform: "translate(36px, 96px)",
-    },
-    country: {
-        fontSize: "16px",
-        transform: "translate(56px, 146px)",
-    }
+
 
 };
 
